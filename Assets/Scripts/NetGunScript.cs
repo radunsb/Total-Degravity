0bb5d05582c778cb1f79379f370958da1d5fd7a7
ncacioppo <ndcacioppo@gmail.com>
2025-04-25 12:55:49 -0400

    Progress
    Changed brightness of arena wall material to make it more visible
    Increased speed and size of the net gun, factoring in velocity of the human (still need to test more)

--- Assets/Scripts/NetGunScript.cs
+++ Assets/Scripts/NetGunScript.cs
@@ -8,15 +8,17 @@ public class NetGunScript : MonoBehaviour
     public GameObject _netProjectilePrefab;
     public Transform _netProjectileSpawnPoint;
     ObjectPool _netProjectilePool;
+    Rigidbody _humanRigidbody;
 
     public float shootCooldown;
+    public float velocityInfluence;
     float shootTime;
 
     // Start is called before the first frame update
     void Start()
     {
         _netProjectilePool = new ObjectPool(_netProjectilePrefab, true, 5);
-
+        _humanRigidbody = GetComponent<Rigidbody>();
         shootTime = 5f;
     }
 
@@ -30,9 +32,11 @@ public class NetGunScript : MonoBehaviour
     {
         if (shootTime >= shootCooldown)
         {
+            Vector3 humanVelocity = _humanRigidbody.velocity;
+
             GameObject net = _netProjectilePool.GetObject();
             net.transform.position = _netProjectileSpawnPoint.position;
-            net.GetComponent<Rigidbody>().velocity = transform.forward * 40;
+            net.GetComponent<Rigidbody>().velocity = (transform.forward * 120) + (humanVelocity * velocityInfluence);
 
             shootTime = 0f;
         }
