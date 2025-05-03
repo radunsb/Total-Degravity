e5e755578071e028e4e6c87b18c86d6d6e7b5c64
NStriebel <StriebelNJ21@gcc.edu>
2025-05-03 16:58:28 -0400

    added arrows to HUD

---
+++ Assets/Scripts/ObjectiveArrowScript.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Camera))]
+public class ObjectiveArrowScript : MonoBehaviour
+{
+    Camera _cam;
+    RectTransform arrowTrans;
+    Transform objective;
+
+    public string arrowTag;
+    public string objectiveTag;
+
+    public float arrowSpeed;
+
+
+    //right, bottom, left, top
+    Vector4 edgePositions;
+
+    float screenWidth;
+    float screenHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objective = GameObject.FindWithTag(objectiveTag).transform;
+        arrowTrans = GameObject.FindWithTag(arrowTag).GetComponent<RectTransform>();
+
+        _cam = GetComponent<Camera>();
+
+        screenWidth = _cam.pixelWidth;
+        screenHeight = _cam.pixelHeight;
+
+        //right, bottom, left, top
+        edgePositions.w = screenWidth - arrowTrans.rect.width/2;
+        edgePositions.x = arrowTrans.rect.height * 1.5f;
+        edgePositions.y = arrowTrans.rect.width/2;
+        edgePositions.z = screenHeight - (arrowTrans.rect.height * 1.5f);
+
+        Debug.Log(edgePositions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //don't do anything unless you can find the objective 
+        if(objective == null)
+        {
+            objective = GameObject.FindWithTag(objectiveTag).transform;
+            return;
+        }
+
+        Vector3 objectivePosition = _cam.WorldToScreenPoint(objective.position);
+
+        bool offRight = objectivePosition.x > screenWidth;
+        bool offLeft = objectivePosition.x < 0;
+        bool offBottom = objectivePosition.y < 0;
+        bool offTop = objectivePosition.y > screenHeight;
+
+        bool onScreen = !offRight && !offLeft && !offTop && !offBottom && objectivePosition.z > 0;
+
+        Debug.LogFormat("ScreenWidth: {0}\nScreenHeight: {1}\nobjectiveScreenPoint: {2}", screenWidth, screenHeight, objectivePosition);
+        Debug.LogFormat("offRight: {0}\nOffLEft: {1}\nOffBottom: {2}\nOffTop: {3}\nOnScreen: {4}", offRight, offLeft, offBottom, offTop, onScreen);
+
+        arrowTrans.gameObject.SetActive(!onScreen);
+
+        if (arrowTrans.gameObject.activeSelf)
+        {
+            float xTarget;
+            float yTarget;
+
+            //rotate arrow to point at objective
+            Vector2 arrowPosition = _cam.WorldToScreenPoint(arrowTrans.position);
+
+            //rotate arrow to point at the objective. z=0 means right, z=90 means up
+            float angleBetween = Vector2.Angle(objectivePosition, Vector2.right);
+            if(offBottom) angleBetween = -angleBetween;
+
+            arrowTrans.localRotation = Quaternion.Euler(new Vector3(0, 0, angleBetween));
+
+            //edgePositions stores (rightEdge, bottomEdge, leftEdge, topEdge)
+            if (offRight) xTarget = edgePositions.w;
+            else if (offLeft) xTarget = edgePositions.y;
+            else xTarget = screenWidth / 2;
+
+            if (offTop) yTarget = edgePositions.z;
+            else if (offBottom) yTarget = edgePositions.x;
+            else yTarget = screenHeight / 2;
+
+            arrowTrans.position = Vector3.Slerp(arrowTrans.position, new Vector3(xTarget, yTarget, 0), arrowSpeed);
+        }
+    }
+}
